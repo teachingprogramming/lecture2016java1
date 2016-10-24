a8e5f184303c01e16e1511cb04931aa31a3124c7
teachingprogramming <teacher@teachingprogramming.net>
2016-10-25 06:34:26 +0900

    Body: BMIを計算するメソッド

--- src/Body.java
+++ src/Body.java
@@ -1,4 +1,10 @@
 class Body {
     double weight; // 体重を表すフィールド
     double height; // 身長を表すフィールド
+
+    // BMIを計算するメソッド
+    double getBmi() {
+        double bmi = weight / (height * height);
+        return bmi;
+    }
 }
