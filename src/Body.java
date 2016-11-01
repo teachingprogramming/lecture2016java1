11320cde9e5fe5b0b85b8a3c706462ead8043f42
teachingprogramming <teacher@teachingprogramming.net>
2016-11-02 05:02:42 +0900

    Lecture04

--- src/Body.java
+++ src/Body.java
@@ -7,4 +7,37 @@ class Body {
         double bmi = weight / (height * height);
         return bmi;
     }
+
+    /**
+     * 身長をセンチでセットするメソッド
+     */
+    void setHeightAsCm(double cm) {
+        height = cm / 100.0;
+    }
+
+    /**
+     * 体重と身長を1度に設定するメソッド
+     */
+    void setWeightAndHeight(double weight, double height) {
+        this.weight = weight;
+        this.height = height;
+    }
+
+    /**
+     * 引数が2つあるコンストラクタ
+     */
+    Body(double weight, double height) {
+        this.weight = weight;
+        this.height = height;
+        // setWeightAndHeight(double weight, double height)
+    }
+
+    /**
+     * 引数が2つあるコンストラクタ
+     */
+    Body() {
+        this.weight = 50.0 + Math.random() * 50.0;
+        this.height = 1.5 + Math.random() * 0.5;
+    }
+
 }
---
+++ src/Lecture04.java
@@ -0,0 +1,20 @@
+class Lecture04 {
+    public static void main(String[] args) {
+        Body taro = new Body();
+        taro.weight = 70.0;
+        taro.height = 1.7;
+        System.out.println("taroのBMIは"+taro.getBmi()+"です。");
+
+        taro.setHeightAsCm(180);
+        System.out.println("taroの身長は"+taro.height+"mです。");
+
+        taro.setWeightAndHeight(60.0, 1.7);
+        System.out.println("taroの身長は"+taro.height+"m、体重は"+taro.weight+"です。");
+
+        Body jiro = new Body(80, 1.7);
+        System.out.println("jiroのBMIは"+jiro.getBmi()+"です。");
+
+        Body hanako = new Body();
+        System.out.println("hanakoのBMIは"+hanako.getBmi()+"です。");
+    }
+}
