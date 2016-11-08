0aef481823cd8e9e93a5a052268ade7c97679aaf
teachingprogramming <teacher@teachingprogramming.net>
2016-11-08 16:36:52 +0900

    Lesson05: 違うパッケージの場合（import使わない）

---
+++ src/Lesson05.java
@@ -0,0 +1,6 @@
+class Lesson05 {
+    public static void main(String[] args) {
+        lesson05.d00000.Book book = new lesson05.d00000.Book("Javaの絵本", "アンク", 1580);
+        book.printInfo();
+    }
+}
--- src/lesson05/d00000/Book.java
+++ src/lesson05/d00000/Book.java
@@ -1,6 +1,6 @@
 package lesson05.d00000;
 
-class Book {
+public class Book {
     String title;
     String author;
     int price;
@@ -8,7 +8,7 @@ class Book {
     /**
      * コンストラクタ
      */
-    Book(String title, String author, int price) {
+    public Book(String title, String author, int price) {
         this.title = title;
         this.author = author;
         this.price = price;
@@ -17,7 +17,7 @@ class Book {
     /**
      * 情報を表示するメソッド
      */
-    void printInfo() {
+    public void printInfo() {
         System.out.println("タイトル: "+title);
         System.out.println("著者: "+author);
         System.out.println("税抜き価格: "+price);
