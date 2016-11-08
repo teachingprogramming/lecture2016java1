1532237397e70ee2bbfaf51779ee055bdebc9d82
teachingprogramming <teacher@teachingprogramming.net>
2016-11-08 16:37:54 +0900

    Lesson05: 違うパッケージの場合（import使う）

--- src/Lesson05.java
+++ src/Lesson05.java
@@ -1,6 +1,8 @@
+import lesson05.d00000.Book;
+
 class Lesson05 {
     public static void main(String[] args) {
-        lesson05.d00000.Book book = new lesson05.d00000.Book("Javaの絵本", "アンク", 1580);
+        Book book = new Book("Javaの絵本", "アンク", 1580);
         book.printInfo();
     }
 }
