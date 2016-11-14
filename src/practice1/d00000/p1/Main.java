d965abb9ef4e2223913470d60f2d965aa1846bce
teachingprogramming <teacher@teachingprogramming.net>
2016-11-14 15:33:48 +0900

    practice1: 利用例

---
+++ src/practice1/d00000/p1/Main.java
@@ -0,0 +1,13 @@
+package practice1.d00000.p1;
+
+public class Main {
+    public static void main(String[] args) {
+        Rectangle r1 = new Rectangle(5, 10);
+        System.out.println("四角r1の面積は"+r1.getArea()+"です。");
+        if (r1.isSquare()) {
+            System.out.println("正方形です。");
+        } else {
+            System.out.println("正方形ではありません。");
+        }
+    }
+}
