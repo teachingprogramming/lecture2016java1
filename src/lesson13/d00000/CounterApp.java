2e825affa2c65390a77d12ab4bcf99876d4084b3
teachingprogramming <teacher@teachingprogramming.net>
2017-01-25 04:54:15 +0900

    lesson13

---
+++ src/lesson13/d00000/CounterApp.java
@@ -0,0 +1,65 @@
+package lesson13.d00000;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class CounterApp extends Application implements EventHandler<ActionEvent> {
+    private Label label;
+    private Button countButton;
+    private Button resetButton;
+    private int count = 0;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // ラベル
+        label = new Label();
+        label.setText("0");
+        label.relocate(10, 10);
+
+        // カウントボタン
+        countButton = new Button();
+        countButton.setText("カウント");
+        countButton.setOnAction(this);
+        countButton.relocate(10, 40);
+
+        // リセットボタン
+        resetButton = new Button();
+        resetButton.setText("リセット");
+        resetButton.setOnAction(this);
+        resetButton.relocate(10, 70);
+
+        // ペイン（レイアウト）
+        Pane pane = new Pane();
+        pane.getChildren().add(label); // ペインにラベルを追加
+        pane.getChildren().add(countButton); // ペインにカウントボタンを追加
+        pane.getChildren().add(resetButton); // ペインにリセットボタンを追加
+
+        // シーン
+        Scene scene = new Scene(pane, 150, 200);
+        primaryStage.setScene(scene); // シーンにペインをセット
+
+        // 最初はこれだけ
+        primaryStage.setTitle("カウンター");
+        primaryStage.show();
+    }
+
+    /**
+     * ボタンがクリックされた時に実行される
+     */
+    @Override
+    public void handle(ActionEvent event) {
+        Object source = event.getSource(); // イベントの発生源となっているインスタンスを取得する
+        if (source.equals(countButton)) {
+            count++;
+        } else if (source.equals(resetButton)) {
+            count = 0;
+        }
+        label.setText(Integer.toString(count));
+    }
+}
---
+++ src/lesson13/d00000/Main.java
@@ -0,0 +1,9 @@
+package lesson13.d00000;
+
+import javafx.application.Application;
+
+public class Main {
+    public static void main(String[] args) {
+        Application.launch(CounterApp.class);
+    }
+}
