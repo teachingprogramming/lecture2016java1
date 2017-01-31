ab0477b6c02ebf7c249442603380770e963ba286
teachingprogramming <teacher@teachingprogramming.net>
2017-01-31 13:00:41 +0900

    lesson13

---
+++ src/lesson13/d00000/OmikujiApp.java
@@ -0,0 +1,63 @@
+package lesson13.d00000;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class OmikujiApp extends Application implements EventHandler<ActionEvent> {
+    private TextField nameTextField;
+    private Button divineButton;
+    private Label resultLabel;
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        nameTextField = new TextField();
+        nameTextField.setText("名前");
+        nameTextField.relocate(10, 10);
+
+        // カウントボタン
+        divineButton = new Button();
+        divineButton.setText("占う");
+        divineButton.relocate(10, 40);
+        divineButton.setOnAction(this);
+
+        // ラベル
+        resultLabel = new Label();
+        resultLabel.setText("結果がここに表示されます。");
+        resultLabel.relocate(10, 70);
+
+        // ペイン（レイアウト）
+        Pane pane = new Pane();
+        pane.getChildren().add(nameTextField);
+        pane.getChildren().add(divineButton);
+        pane.getChildren().add(resultLabel);
+
+        // シーン
+        Scene scene = new Scene(pane, 250, 200);
+        primaryStage.setScene(scene); // シーンにペインをセット
+
+        // 最初はこれだけ
+        primaryStage.setTitle("カウンター");
+        primaryStage.show();
+    }
+
+    @Override
+    public void handle(ActionEvent event) {
+        int rand = (int)Math.floor(Math.random()*3.0); // 0, 1, 2
+        String result = "";
+        if (rand == 0) {
+            result = "大吉";
+        } else if (rand == 1) {
+            result = "吉";
+        } else { // rand == 0
+            result = "凶";
+        }
+        String name = nameTextField.getText();
+        resultLabel.setText(name + "さんの今日の運勢は" + result);
+    }
+}
